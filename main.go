6c092edf2342bae81bad525cdae9581560c896f4
Dmitry Mishin <dmitry.mishin@gmail.com>
2017-09-21 18:23:31 -0700

    Multiple changes for removing and copying stuff

--- main.go
+++ main.go
@@ -9,16 +9,23 @@ import (
 
 	"github.com/Songmu/prompter"
 	//https://groups.google.com/forum/#!topic/golang-nuts/Mwn9buVnLmY
+	"github.com/dustin/go-humanize"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	delFlag         = kingpin.Flag("delete", "DELETE the folder provided WITH ALL ITS CONTENTS").Short('d').Bool()
-	cpFlag          = kingpin.Flag("copy", "Copy the folder provided").Short('c').Bool()
 	fileWorkersFlag = kingpin.Flag("fworkers", "Number of file copy workers per folder").Default("4").Short('w').Int()
 	dirWorkersFlag  = kingpin.Flag("dworkers", "Number of dir copy workers per folder").Default("2").Short('e').Int()
-	dirParam        = kingpin.Arg("dir", "Folder in Lustre to process").Required().ExistingDir()
-	targetParam     = kingpin.Arg("tdir", "Folder in Lustre to copy to").ExistingDir()
+
+	countCommand  = kingpin.Command("count", "Count the number of files and folders")
+	countDirParam = countCommand.Arg("dir", "Folder in Lustre").Required().ExistingDir()
+
+	rmCommand  = kingpin.Command("rm", "DELETE the folder with all its contents")
+	rmDirParam = rmCommand.Arg("dir", "Folder in Lustre to delete").Required().ExistingDir()
+
+	cpCommand     = kingpin.Command("cp", "Copy the folder with all its contents")
+	cpSourceParam = cpCommand.Arg("source", "Source folder in Lustre").Required().ExistingDir()
+	cpTargetParam = cpCommand.Arg("target", "Target folder in Lustre").Required().ExistingDir()
 )
 
 var (
@@ -29,36 +36,25 @@ var (
 	bytes          uint64
 )
 
-var srcDir string
-var targetDir string
+var srcRootDir string
+var destRootDir string
+
+var command string
 
 func main() {
 	kingpin.Version("1.0").Author("Dmitry Mishin <dmishin@sdsc.edu>")
-	kingpin.CommandLine.Help = "A command-line tool to scan or delete huge data collections in Lustre.\nBy default scans the folder and displays the number of files and folders found."
+	kingpin.CommandLine.Help = "A command-line tool for managing huge data collections in Lustre."
 	kingpin.CommandLine.HelpFlag.Short('h')
 
-	kingpin.Parse()
+	command = kingpin.Parse()
 
-	var err error
-	srcDir, err = filepath.Abs(*dirParam)
-	if err != nil {
-		fmt.Printf("Error finding the abs src folder: %s", err.Error())
-		return
-	}
-	targetDir, err = filepath.Abs(*targetParam)
-	if err != nil {
-		fmt.Printf("Error finding the abs target folder: %s", err.Error())
-		return
-	}
-
-	if *delFlag {
-		absPath, err := filepath.Abs(*dirParam)
+	switch command {
+	case rmCommand.FullCommand():
+		sourceFullPath, err := filepath.Abs(*rmDirParam)
 		if err != nil {
-			fmt.Printf("Error finding the folder: %s", err.Error())
-			return
+			log.Fatalf("Error path: %s", err.Error())
 		}
-
-		if !prompter.YN(fmt.Sprintf("Do you really want to DELETE EVERYTHING in %s", absPath), false) {
+		if !prompter.YN(fmt.Sprintf("Do you really want to DELETE EVERYTHING in %s", sourceFullPath), false) {
 			return
 		}
 	}
@@ -82,7 +78,24 @@ func main() {
 		}
 	}()
 
-	err = processDir(*dirParam)
+	var err, err1 error
+	switch command {
+	case rmCommand.FullCommand():
+		srcRootDir, err = filepath.Abs(*rmDirParam)
+	case cpCommand.FullCommand():
+		srcRootDir, err = filepath.Abs(*cpSourceParam)
+		destRootDir, err1 = filepath.Abs(*cpTargetParam)
+	case countCommand.FullCommand():
+		srcRootDir, err = filepath.Abs(*countDirParam)
+	}
+	if err != nil {
+		log.Fatalf("Error path: %s", err.Error())
+	}
+	if err1 != nil {
+		log.Fatalf("Error path: %s", err.Error())
+	}
+
+	err = processDir(srcRootDir)
 	if err != nil {
 		log.Printf("Error: %s", err.Error())
 	}
@@ -92,11 +105,12 @@ func main() {
 }
 
 func printStatus() {
-	if *delFlag {
+	switch command {
+	case rmCommand.FullCommand():
 		fmt.Printf("\rScanned Files = %v; Deleted Files = %v; Scanned Dirs = %v; Deleted Dirs = %v;", totalFiles, processedFiles, totalDirs, processedDirs)
-	} else if *cpFlag {
-		fmt.Printf("\rScanned Files = %v; Copied Files = %v; Scanned Dirs = %v; Copied Dirs = %v; Bytes transferred = %v", totalFiles, processedFiles, totalDirs, processedDirs, bytes)
-	} else {
+	case cpCommand.FullCommand():
+		fmt.Printf("\rScanned Files = %v; Copied Files = %v; Scanned Dirs = %v; Copied Dirs = %v; Bytes transferred = %s", totalFiles, processedFiles, totalDirs, processedDirs, humanize.Bytes(bytes))
+	case countCommand.FullCommand():
 		fmt.Printf("\rScanned Files = %v; Scanned Dirs = %v;", totalFiles, totalDirs)
 	}
 }
--- process.go
+++ process.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"bufio"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"sync"
 	"sync/atomic"
+	"syscall"
 
 	"github.com/karalabe/bufioprop"
 )
 
-func processDir(dir string) error {
-	log.Printf("Started: %s", dir)
-	dirsChan := make(chan string, 1000001)
-	filesChan := make(chan string, 1000001)
+const LustreNoStripeSize = 10 * 1000000000
+const Lustre5StripeSize = 100 * 1000000000
+const Lustre10StripeSize = 1000 * 1000000000
 
+func processDir(dir string) error {
+	// log.Printf("Started: %s", dir)
 	var wg sync.WaitGroup
 
 	absPath, err := filepath.Abs(dir)
@@ -24,158 +27,323 @@ func processDir(dir string) error {
 		return err
 	}
 
-	if srcDir != absPath {
-		relPath, err := filepath.Rel(srcDir, dir)
-		if err != nil {
-			log.Print("Error finding relative path for ", dir, ": ", err.Error())
+	if command == cpCommand.FullCommand() {
+		if err = makeDestDir(absPath); err != nil {
 			return err
 		}
-		destDir := filepath.Join(*targetParam, relPath)
-		srcMeta, err := os.Lstat(absPath)
-		if err != nil {
-			log.Print("Error reading source folder meta ", absPath, ": ", err.Error())
-			return err
-		}
-
-		os.Mkdir(destDir, srcMeta.Mode())
 	}
 
-	cmdDirName := "lfs"
-	cmdDirArgs := []string{"find", absPath, "-maxdepth", "1", "-type", "d"}
-
-	cmdDir := exec.Command(cmdDirName, cmdDirArgs...)
-	cmdDirReader, err := cmdDir.StdoutPipe()
-	if err != nil {
-		log.Printf("Error running lustre find: %v", err.Error())
-		return err
-	}
-
-	scannerDir := bufio.NewScanner(cmdDirReader)
-
-	err = cmdDir.Start()
+	err = findFolders(absPath, &wg)
 	if err != nil {
 		return err
 	}
+
+	err = findFiles(absPath, &wg)
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+
+	// After processing the folder is done, can remove one if needed
+	if command == rmCommand.FullCommand() {
+		// log.Printf("Deleting dir %s", absPath)
+		if err = os.Remove(absPath); err != nil {
+			log.Println(err.Error())
+		}
+	}
+
+	atomic.AddUint64(&processedDirs, uint64(1))
+	// log.Printf("Done with: %s", dir)
+	return nil
+}
+
+func makeDestDir(dir string) error {
+	if srcRootDir != dir {
+		relPath, err := filepath.Rel(srcRootDir, dir)
+		if err != nil {
+			return err
+		}
+		destDir := filepath.Join(destRootDir, relPath)
+		srcMeta, err := os.Lstat(dir)
+		if err != nil {
+			return err
+		}
+
+		if destDirMeta, err := os.Lstat(destDir); err == nil { // the dest folder exists
+			sourceDirStat := srcMeta.Sys().(*syscall.Stat_t)
+			sourceDirUid := int(sourceDirStat.Uid)
+			sourceDirGid := int(sourceDirStat.Gid)
+
+			destDirStat := destDirMeta.Sys().(*syscall.Stat_t)
+			destDirUid := int(destDirStat.Uid)
+			destDirGid := int(destDirStat.Gid)
+
+			if destDirMeta.Mode() != srcMeta.Mode() {
+				if err = os.Chmod(destDir, srcMeta.Mode()); err != nil {
+					return err
+				}
+			}
+
+			if sourceDirUid != destDirUid || sourceDirGid != destDirGid {
+				if err = os.Lchown(destDir, sourceDirUid, sourceDirGid); err != nil {
+					return err
+				}
+			}
+		} else {
+			if err = os.Mkdir(destDir, srcMeta.Mode()); err != nil {
+				return err
+			}
+			if err = os.Lchown(destDir, int(srcMeta.Sys().(*syscall.Stat_t).Uid), int(srcMeta.Sys().(*syscall.Stat_t).Gid)); err != nil {
+				return err
+			}
+		}
+
+	}
+	return nil
+}
+
+func findFolders(dir string, wg *sync.WaitGroup) error {
+	dirsChan := make(chan string, 1000001)
 
 	wg.Add(1)
 	go func() {
 		defer close(dirsChan)
 		defer wg.Done()
 
+		cmdDirName := "lfs"
+		cmdDirArgs := []string{"find", dir, "-maxdepth", "1", "-type", "d"}
+
+		cmdDir := exec.Command(cmdDirName, cmdDirArgs...)
+		cmdDirReader, err := cmdDir.StdoutPipe()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		stderr, err := cmdDir.StderrPipe()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		scannerDir := bufio.NewScanner(cmdDirReader)
+
+		err = cmdDir.Start()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
 		for scannerDir.Scan() {
 			newDir := scannerDir.Text()
-			if newDir != absPath {
+			if newDir != dir {
 				dirsChan <- newDir
 				atomic.AddUint64(&totalDirs, uint64(1))
 			}
 		}
-		cmdDir.Wait()
+
+		slurp, _ := ioutil.ReadAll(stderr)
+
+		if err := cmdDir.Wait(); err != nil {
+			log.Printf("%s %s", err.Error(), slurp)
+		}
 	}()
 
 	for i := 0; i < *dirWorkersFlag; i++ {
+		wg.Add(1)
 		go func() {
-			wg.Add(1)
 			defer wg.Done()
 			for dir := range dirsChan {
-				err = processDir(dir)
+				err := processDir(dir)
 				if err != nil {
-					log.Printf("Error processing folder %s: %v", dir, err.Error())
+					log.Println(err.Error())
+					return
 				}
 			}
 		}()
 	}
+	return nil
+}
 
-	cmdFileName := "lfs"
-	cmdFileArgs := []string{"find", absPath, "-maxdepth", "1", "!", "-type", "d"}
-
-	cmdFile := exec.Command(cmdFileName, cmdFileArgs...)
-	cmdFileReader, err := cmdFile.StdoutPipe()
-	if err != nil {
-		log.Printf("Error running lustre find: %v", err.Error())
-		return err
-	}
-
-	scannerFile := bufio.NewScanner(cmdFileReader)
-
-	err = cmdFile.Start()
-	if err != nil {
-		return err
-	}
+func findFiles(dir string, wg *sync.WaitGroup) error {
+	filesChan := make(chan string, 1000001)
 
 	wg.Add(1)
 	go func() {
 		defer close(filesChan)
 		defer wg.Done()
 
+		cmdFileName := "lfs"
+		cmdFileArgs := []string{"find", dir, "-maxdepth", "1", "!", "-type", "d"}
+
+		cmdFile := exec.Command(cmdFileName, cmdFileArgs...)
+
+		cmdFileReader, err := cmdFile.StdoutPipe()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		stderr, err := cmdFile.StderrPipe()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		scannerFile := bufio.NewScanner(cmdFileReader)
+
+		err = cmdFile.Start()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
 		for scannerFile.Scan() {
 			newFile := scannerFile.Text()
 			filesChan <- newFile
 			atomic.AddUint64(&totalFiles, uint64(1))
 		}
-		cmdFile.Wait()
+
+		slurp, _ := ioutil.ReadAll(stderr)
+
+		if err := cmdFile.Wait(); err != nil {
+			log.Printf("%s %s", err.Error(), slurp)
+		}
 	}()
 
-	for i := 0; i < *fileWorkersFlag; i++ {
-		go func() {
+	if command == cpCommand.FullCommand() || command == rmCommand.FullCommand() {
+		for i := 0; i < *fileWorkersFlag; i++ {
 			wg.Add(1)
-			defer wg.Done()
-			for file := range filesChan {
-				if *delFlag {
-					os.Remove(file)
-				} else if *cpFlag {
-					log.Printf("Copying: %s", file)
-					srcMeta, err := os.Lstat(file)
-					if err != nil {
-						log.Print("Error reading source file meta ", file, ": ", err.Error())
-						continue
-					}
+			go func() {
+				defer wg.Done()
+				for file := range filesChan {
 
-					switch mode := srcMeta.Mode(); {
-					case mode.IsRegular():
-						src, err := os.Open(file)
+					switch command {
+					case rmCommand.FullCommand():
+						if err := os.Remove(file); err != nil {
+							log.Println(err.Error())
+						}
+					case cpCommand.FullCommand():
+						// log.Printf("Copying: %s", file)
+						srcMeta, err := os.Lstat(file)
 						if err != nil {
-							log.Print("Error opening src file ", file, ": ", err.Error())
+							log.Print("Error reading source file meta ", file, ": ", err.Error())
 							continue
 						}
 
-						relPath, err := filepath.Rel(srcDir, file)
+						relPath, err := filepath.Rel(srcRootDir, file)
 						if err != nil {
 							log.Print("Error finding relative path for ", file, ": ", err.Error())
 							continue
 						}
-						destFile := filepath.Join(*targetParam, relPath)
-						dest, err := os.Create(destFile)
-						if err != nil {
-							log.Print("Error opening dst file ", file, ": ", err.Error())
-							src.Close()
-							continue
-						}
-						bytesCopied, err := bufioprop.Copy(dest, src, 1048559)
-						if err != nil {
-							log.Print("Error copying file ", file, ": ", err.Error())
+						destFile := filepath.Join(destRootDir, relPath)
+
+						switch mode := srcMeta.Mode(); {
+						case mode.IsRegular():
+							if dstMeta, err := os.Lstat(destFile); err == nil { // the dest file exists
+								if srcMeta.Size() == dstMeta.Size() &&
+									srcMeta.Mode() == dstMeta.Mode() &&
+									srcMeta.ModTime() == dstMeta.ModTime() {
+									atomic.AddUint64(&processedFiles, uint64(1))
+									continue
+								} else {
+									if err = os.Remove(destFile); err != nil {
+										log.Println(err.Error())
+										continue
+									}
+								}
+								log.Printf("File %s exists and is modified ", destFile)
+							}
+
+							src, err := os.Open(file)
+							if err != nil {
+								log.Print("Error opening src file ", file, ": ", err.Error())
+								continue
+							}
+
+							if srcMeta.Size() > LustreNoStripeSize {
+								cmdName := "/usr/bin/lfs"
+								var cmdArgs []string
+								if srcMeta.Size() < Lustre5StripeSize {
+									cmdArgs = []string{"setstripe", "-c", "5", destFile}
+								} else if srcMeta.Size() < Lustre10StripeSize {
+									cmdArgs = []string{"setstripe", "-c", "10", destFile}
+								} else {
+									cmdArgs = []string{"setstripe", "-c", "50", destFile}
+								}
+
+								if _, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+									log.Println(err.Error())
+									continue
+								}
+							}
+
+							dest, err := os.OpenFile(destFile, os.O_CREATE|os.O_RDWR|os.O_TRUNC, srcMeta.Mode())
+							if err != nil {
+								log.Print("Error opening dst file ", file, ": ", err.Error())
+								src.Close()
+								continue
+							}
+
+							bytesCopied, err := bufioprop.Copy(dest, src, 1048559)
+							if err != nil {
+								log.Print("Error copying file ", file, ": ", err.Error())
+								src.Close()
+								dest.Close()
+								continue
+							}
 							src.Close()
 							dest.Close()
-							continue
+
+							sourceMtime := srcMeta.ModTime()
+							sourceAtime := getAtime(srcMeta.Sys().(*syscall.Stat_t))
+
+							if err = os.Lchown(destFile, int(srcMeta.Sys().(*syscall.Stat_t).Uid), int(srcMeta.Sys().(*syscall.Stat_t).Gid)); err != nil {
+								log.Println(err.Error())
+							}
+							if err = os.Chmod(destFile, srcMeta.Mode()); err != nil {
+								log.Println(err.Error())
+							}
+							if err = os.Chtimes(destFile, sourceAtime, sourceMtime); err != nil {
+								log.Println(err.Error())
+							}
+
+							atomic.AddUint64(&bytes, uint64(bytesCopied))
+							// log.Printf("Done copying: %s", file)
+						case mode&os.ModeSymlink != 0:
+							linkTarget, err := os.Readlink(file)
+							if err != nil {
+								log.Print("Error reading symlink ", file, ": ", err)
+								continue
+							}
+
+							if _, err = os.Lstat(destFile); err == nil { // the dest link exists
+								if dlinkTarget, err := os.Readlink(destFile); err == nil && dlinkTarget == linkTarget {
+									atomic.AddUint64(&processedFiles, uint64(1))
+									continue
+								}
+
+								if err = os.Remove(destFile); err != nil {
+									log.Print("Error removing symlink ", destFile, ": ", err)
+									continue
+								}
+							}
+
+							if err = os.Symlink(linkTarget, destFile); err != nil {
+								log.Print("Error seting symlink ", destFile, ": ", err)
+								continue
+							}
+							if err = os.Lchown(destFile, int(srcMeta.Sys().(*syscall.Stat_t).Uid), int(srcMeta.Sys().(*syscall.Stat_t).Gid)); err != nil {
+								log.Println(err.Error())
+							}
 						}
-						src.Close()
-						dest.Close()
-						atomic.AddUint64(&bytes, uint64(bytesCopied))
-						log.Printf("Done copying: %s", file)
 					}
+					atomic.AddUint64(&processedFiles, uint64(1))
 				}
-				atomic.AddUint64(&processedFiles, uint64(1))
-			}
-		}()
+			}()
+		}
 	}
 
-	wg.Wait()
-
-	if *delFlag {
-		// log.Printf("Deleting dir %s", absPath)
-		os.Remove(absPath)
-	}
-
-	atomic.AddUint64(&processedDirs, uint64(1))
-	log.Printf("Done with: %s", dir)
 	return nil
 }
